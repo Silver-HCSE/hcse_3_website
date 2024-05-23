52d5b2d1b4d1a958ff518a364f2d90c51510c7fa
Pascal Kraft <kraft.pascal@gmail.com>
2024-05-23 16:06:00 +0200

    Migrate to zoneless.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { provideExperimentalZonelessChangeDetection } from '@angular/core' ;
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppModule, {
+providers: [
+    provideExperimentalZonelessChangeDetection()
+  ]
+})
   .catch(err => console.error(err));
