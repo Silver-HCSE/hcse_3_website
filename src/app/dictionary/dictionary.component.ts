c6c958e8891859280d7a78d838ed0daf6e9b9603
Pascal Kraft <kraft.pascal@gmail.com>
2024-09-21 15:02:51 +0200

    Remove test files, keyword dictionay component.

--- src/app/dictionary/dictionary.component.ts
+++ src/app/dictionary/dictionary.component.ts
@@ -1,28 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Signal, WritableSignal, computed, signal } from '@angular/core';
 import { HcseDataService } from '../hcse-data.service';
+import { KeywordRating } from '../util';
+import { FormsModule } from '@angular/forms';
+import { KeywordRatingComponent } from '../keyword-rating/keyword-rating.component';
+
+const MAX_WORDS = 50;
 
 @Component({
   selector: 'app-dictionary',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, KeywordRatingComponent],
   templateUrl: './dictionary.component.html',
   styleUrl: './dictionary.component.scss'
 })
 export class DictionaryComponent {
-  current_letter: string = 'A';
-  words: any[] = [];
+  term: WritableSignal<string> = signal("");
+  keywords: Signal<KeywordRating[]> = computed(() => {
+    let ret: KeywordRating[] = [];
+    let results: number = 0;
+    for (let key in this.data.keyword_ratings()) {
+      if (key.indexOf(this.term()) != -1) {
+        const new_item: KeywordRating = {
+          keyword: key,
+          rating: this.data.keyword_ratings()[key]
+        }
+        ret.push(new_item);
+        results++;
+      }
+    }
+    ret.sort((a, b) => {
+      if (a.keyword < b.keyword) return -1;
+      if (a.keyword > b.keyword) return 1;
+      return 0;
+    });
+
+    return ret.slice(0, MAX_WORDS);
+  });
+
+  count: Signal<number> = computed(() => {
+    return this.keywords().length;
+  });
+
+  count_string: Signal<string> = computed(() => {
+    let ret: string = this.count().toString();
+    if (this.count() == MAX_WORDS) {
+      ret += "+";
+    }
+    return ret;
+  });
+
+
+
   constructor(private data: HcseDataService) {
 
   }
 
-  update_word_list(new_char: string) {
-    this.current_letter = new_char[0];
-    const dictionary = this.data.keyword_ratings();
-    this.words = [];
-    for (let key in dictionary) {
-      if (key[0] == this.current_letter) {
-        this.words.push(dictionary[key]);
-      }
-    }
-  }
 }
